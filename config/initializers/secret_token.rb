c999794a2a99dbe6dc65f7dc396f70f22c9d6793
caok <caok1231@gmail.com>
2013-11-13 14:00:58 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ilovemyhouse::Application.config.secret_key_base = '9bb6f642e5bede3c46db5a6ec084f623789a0a096cf3ba0796f02a62fb2c12215a88c83982be57577aaa4c8b86cad0a425b696a4d7a902e7cff48db53e6a129e'
