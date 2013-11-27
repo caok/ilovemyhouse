c5c7584dc0ede7cd6b843c4255af226fcb4087f8
caok <caok1231@gmail.com>
2013-11-27 10:07:02 +0800

    remove nginx.conf from deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,7 @@ namespace :deploy do
   end
 
   task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    #sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.yml.example"), "#{shared_path}/config/database.yml"
   end
