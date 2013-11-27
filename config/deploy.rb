256c2e8a4c0c7217f0c5a81a42fd84444ee6f344
caok <caok1231@gmail.com>
2013-11-27 12:24:06 +0800

    fix some bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,3 +75,9 @@ namespace :carrierwave do
 end
 
 after 'deploy:update', 'carrierwave:symlink'
+
+desc 'copy ckeditor nondigest assets'
+task :copy_nondigest_assets, roles: :app do
+  run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} ckeditor:copy_nondigest_assets"
+end
+after 'deploy:assets:precompile', 'copy_nondigest_assets'
