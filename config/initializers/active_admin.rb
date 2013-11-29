473bc33375bab332d7d0acf1bdd887a818a7a260
caok <caok1231@gmail.com>
2013-11-29 21:00:22 +0800

    fix something

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Ilovemyhouse"
+  config.site_title = "我爱我家"
 
   # select languages
   config.namespace :admin do |admin|
